ba6e9d9d4ad920be583f04b5d5dca147e1ae4419
João Pedro <55640904+joaopedroleonel@users.noreply.github.com>
2021-09-21 13:10:26 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sample-site
-this site was created with the sole purpose of showing my knowledge in html css and javascript
+this site was made for the sole purpose of showing my knowledge in html , css and javascript
+<br>
+<br>
+Desktop:
+<br>
+<img src="print1.png"></img>
+<br>
+<br>
+Cell phone:
+<br>
+<img src="print2.png"></img>
---
+++ anima.js
@@ -0,0 +1,227 @@
+function New() {
+			document.getElementById("pizza2").style.visibility = "hidden";
+			document.getElementById("pizza").style.visibility = "hidden";
+			document.getElementById("pizza1").style.visibility = "visible";
+			document.getElementById("1").style.visibility = "hidden";
+			document.getElementById("3").style.visibility = "hidden";
+			document.getElementById("2").style.visibility = "visible";
+			document.getElementById("p1").style.visibility = "hidden";
+			document.getElementById("p2").style.visibility = "hidden";
+			document.getElementById("p3").style.visibility = "hidden";
+			document.getElementById("p1t").style.visibility = "hidden";
+			document.getElementById("h1").style.visibility = "hidden";
+			document.getElementById("ha").style.visibility = "hidden";
+			document.getElementById("p2t").style.visibility = "hidden";
+			document.getElementById("h2").style.visibility = "hidden";
+			document.getElementById("haa").style.visibility = "hidden";
+			document.getElementById("p3t").style.visibility = "hidden";
+			document.getElementById("h3").style.visibility = "hidden";
+			document.getElementById("haaa").style.visibility = "hidden";
+			document.getElementById("pizza").style.animation = "none";
+			document.getElementById("pizza2").style.animation = "none";
+			document.getElementById("rodape").style.marginTop = "130px";
+			document.getElementById("p1").style.animation = "none";
+			document.getElementById("p1t").style.animation = "none";
+			document.getElementById("h1").style.animation = "none";
+			document.getElementById("ha").style.animation = "none";
+			document.getElementById("p3").style.animation = "none";
+			document.getElementById("p3t").style.animation = "none";
+			document.getElementById("h3").style.animation = "none";
+			document.getElementById("haaa").style.animation = "none";
+			document.getElementById("p2").style.animation = "none";
+			document.getElementById("p2t").style.animation = "none";
+			document.getElementById("h2").style.animation = "none";
+			document.getElementById("haa").style.animation = "none";
+		}
+
+		function vai() {
+		  if (x.matches) { 
+		    document.getElementById("rodape").style.marginTop = "2000px";
+		  } else {
+		    document.getElementById("rodape").style.marginTop = "430px";
+		  }
+		}
+
+		var x = window.matchMedia("(max-width: 1020px)")
+		
+		function anima() {
+			document.getElementById("pizza1").style.animation = "mynewmove 1s reverse";
+		}
+		
+		function New2() {
+			document.getElementById("pizza").style.visibility = "visible";
+			document.getElementById("pizza2").style.visibility = "hidden";
+			document.getElementById("pizza1").style.visibility = "hidden";
+			document.getElementById("1").style.visibility = "visible";
+			document.getElementById("2").style.visibility = "hidden";
+			document.getElementById("3").style.visibility = "hidden";
+			document.getElementById("p1").style.visibility = "hidden";
+			document.getElementById("p2").style.visibility = "hidden";
+			document.getElementById("p3").style.visibility = "hidden";
+			document.getElementById("p1t").style.visibility = "hidden";
+			document.getElementById("h1").style.visibility = "hidden";
+			document.getElementById("ha").style.visibility = "hidden";
+			document.getElementById("p2t").style.visibility = "hidden";
+			document.getElementById("h2").style.visibility = "hidden";
+			document.getElementById("haa").style.visibility = "hidden";
+			document.getElementById("p3t").style.visibility = "hidden";
+			document.getElementById("h3").style.visibility = "hidden";
+			document.getElementById("haaa").style.visibility = "hidden";
+			document.getElementById("pizza1").style.animation = "none";
+			document.getElementById("pizza2").style.animation = "none";
+			document.getElementById("rodape").style.marginTop = "130px";
+			document.getElementById("p1").style.animation = "none";
+			document.getElementById("p1t").style.animation = "none";
+			document.getElementById("h1").style.animation = "none";
+			document.getElementById("ha").style.animation = "none";
+			document.getElementById("p3").style.animation = "none";
+			document.getElementById("p3t").style.animation = "none";
+			document.getElementById("h3").style.animation = "none";
+			document.getElementById("haaa").style.animation = "none";
+			document.getElementById("p2").style.animation = "none";
+			document.getElementById("p2t").style.animation = "none";
+			document.getElementById("h2").style.animation = "none";
+			document.getElementById("haa").style.animation = "none";
+		}
+		function anima2() {
+			document.getElementById("pizza").style.animation = "mynewmove 1s reverse";
+		}
+
+		function New3() {
+			document.getElementById("pizza2").style.visibility = "visible";
+			document.getElementById("pizza1").style.visibility = "hidden";
+			document.getElementById("pizza").style.visibility = "hidden";
+			document.getElementById("1").style.visibility = "hidden";
+			document.getElementById("2").style.visibility = "hidden";
+			document.getElementById("3").style.visibility = "visible";
+			document.getElementById("p1").style.visibility = "hidden";
+			document.getElementById("p2").style.visibility = "hidden";
+			document.getElementById("p3").style.visibility = "hidden";
+			document.getElementById("p1t").style.visibility = "hidden";
+			document.getElementById("h1").style.visibility = "hidden";
+			document.getElementById("ha").style.visibility = "hidden";
+			document.getElementById("p2t").style.visibility = "hidden";
+			document.getElementById("h2").style.visibility = "hidden";
+			document.getElementById("haa").style.visibility = "hidden";
+			document.getElementById("p3t").style.visibility = "hidden";
+			document.getElementById("h3").style.visibility = "hidden";
+			document.getElementById("haaa").style.visibility = "hidden";
+			document.getElementById("pizza1").style.animation = "none";	
+			document.getElementById("pizza").style.animation = "none";
+			document.getElementById("rodape").style.marginTop = "130px";
+			document.getElementById("p1").style.animation = "none";
+			document.getElementById("p1t").style.animation = "none";
+			document.getElementById("h1").style.animation = "none";
+			document.getElementById("ha").style.animation = "none";
+			document.getElementById("p3").style.animation = "none";
+			document.getElementById("p3t").style.animation = "none";
+			document.getElementById("h3").style.animation = "none";
+			document.getElementById("haaa").style.animation = "none";
+			document.getElementById("p2").style.animation = "none";
+			document.getElementById("p2t").style.animation = "none";
+			document.getElementById("h2").style.animation = "none";
+			document.getElementById("haa").style.animation = "none";
+		} 
+		function anima3() {
+			document.getElementById("pizza2").style.animation = "mynewmove 1s reverse";
+		}
+
+		function newaa() {
+			document.getElementById("1").style.visibility = "hidden";
+			document.getElementById("2").style.visibility = "hidden";
+			document.getElementById("3").style.visibility = "hidden";
+			document.getElementById("p1").style.visibility = "visible";
+			document.getElementById("p1t").style.visibility = "visible";
+			document.getElementById("h1").style.visibility = "visible";
+			document.getElementById("ha").style.visibility = "visible";
+			document.getElementById("p2").style.visibility = "hidden";
+			document.getElementById("p3").style.visibility = "hidden";
+			document.getElementById("p2t").style.visibility = "hidden";
+			document.getElementById("h2").style.visibility = "hidden";
+			document.getElementById("haa").style.visibility = "hidden";
+			document.getElementById("p3t").style.visibility = "hidden";
+			document.getElementById("h3").style.visibility = "hidden";
+			document.getElementById("haaa").style.visibility = "hidden";
+			document.getElementById("rodape").style.marginTop='430px';
+		}
+		
+		function ani() {
+			document.getElementById("p1").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("p1t").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("h1").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("ha").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("p2").style.animation = "none";
+			document.getElementById("p2t").style.animation = "none";
+			document.getElementById("h2").style.animation = "none";
+			document.getElementById("haa").style.animation = "none";
+			document.getElementById("p3").style.animation = "none";
+			document.getElementById("p3t").style.animation = "none";
+			document.getElementById("h3").style.animation = "none";
+			document.getElementById("haaa").style.animation = "none";
+		}
+		function newaa2() {
+			document.getElementById("p2").style.visibility = "visible";
+			document.getElementById("p1").style.visibility = "hidden";
+			document.getElementById("p3").style.visibility = "hidden";
+			document.getElementById("1").style.visibility = "hidden";
+			document.getElementById("2").style.visibility = "hidden";
+			document.getElementById("3").style.visibility = "hidden";
+			document.getElementById("p3").style.visibility = "hidden";
+			document.getElementById("p1t").style.visibility = "hidden";
+			document.getElementById("h1").style.visibility = "hidden";
+			document.getElementById("ha").style.visibility = "hidden";
+			document.getElementById("p2t").style.visibility = "visible";
+			document.getElementById("h2").style.visibility = "visible";
+			document.getElementById("haa").style.visibility = "visible";
+			document.getElementById("p3t").style.visibility = "hidden";
+			document.getElementById("h3").style.visibility = "hidden";
+			document.getElementById("haaa").style.visibility = "hidden";
+			document.getElementById("rodape").style.marginTop='430px';
+		}
+		function ani2() {
+			document.getElementById("p2").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("p2t").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("h2").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("haa").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("p1").style.animation = "none";
+			document.getElementById("p1t").style.animation = "none";
+			document.getElementById("h1").style.animation = "none";
+			document.getElementById("ha").style.animation = "none";
+			document.getElementById("p3").style.animation = "none";
+			document.getElementById("p3t").style.animation = "none";
+			document.getElementById("h3").style.animation = "none";
+			document.getElementById("haaa").style.animation = "none";
+		}
+
+		function newaa3() {
+			document.getElementById("p1").style.visibility = "hidden";
+			document.getElementById("p3").style.visibility = "visible";
+			document.getElementById("p2").style.visibility = "hidden";
+			document.getElementById("1").style.visibility = "hidden";
+			document.getElementById("2").style.visibility = "hidden";
+			document.getElementById("3").style.visibility = "hidden";
+			document.getElementById("p1t").style.visibility = "hidden";
+			document.getElementById("h1").style.visibility = "hidden";
+			document.getElementById("ha").style.visibility = "hidden";
+			document.getElementById("p2t").style.visibility = "hidden";
+			document.getElementById("h2").style.visibility = "hidden";
+			document.getElementById("haa").style.visibility = "hidden";
+			document.getElementById("p3t").style.visibility = "visible";
+			document.getElementById("h3").style.visibility = "visible";
+			document.getElementById("haaa").style.visibility = "visible";
+			document.getElementById("rodape").style.marginTop='430px';
+		}
+		function ani3() {
+			document.getElementById("p3").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("p3t").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("h3").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("haaa").style.animation = "mynewmove 0.5s reverse";
+			document.getElementById("p1").style.animation = "none";
+			document.getElementById("p1t").style.animation = "none";
+			document.getElementById("h1").style.animation = "none";
+			document.getElementById("ha").style.animation = "none";
+			document.getElementById("p2").style.animation = "none";
+			document.getElementById("p2t").style.animation = "none";
+			document.getElementById("h2").style.animation = "none";
+			document.getElementById("haa").style.animation = "none";
+		}
